b1d13d850f3658b41259cfbabaf26e6489f0a42e
ShiroAky <73247790+ShiroAky@users.noreply.github.com>
2025-08-24 02:12:36 -0400

    update: Firts upload

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { invoke } from "@tauri-apps/api/core";
+import { check } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
+
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
+
+async function checkForUpdates() {
+    const update = await check();
+    if (update) {
+      console.log(`Nueva versión: ${update.version}`);
+      await update.downloadAndInstall();
+      await relaunch();
+    }
+  }
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+
+  checkForUpdates();
+});
